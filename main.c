480853506f794820a4b448f1d555d3a6d228aded
pbalykov <p.d.balykov@gmail.com>
2021-11-09 09:44:32 +0300

    Game

--- main.c
+++ main.c
@@ -11,8 +11,8 @@ int main(){
 	srand(time(NULL));
 	int max = 0, len = 4, sum = 0;
 	int** field = creat_field(len);
+	char flag_sign;
 	while (1){
-		int flag = 1;
 		cout_max_and_sum((max = MAX(max_filed(field, len), max)) ,sum);
 		printf_field(field, len);
 		if (!check_moves(field, len) || max == 2048)
@@ -30,24 +30,24 @@ int main(){
 			clear(4);
 			continue;
 		}
-		char sign = input();
-		if (sign == 'a' || sign == 'A')
-			sum += shift_left(field, len);
-		else if (sign == 'd' || sign == 'D')
-			sum += shift_right(field, len);
-		else if (sign == 's' || sign == 'S')
-			sum += shift_down(field, len);
-		else if (sign == 'w' || sign == 'W')
-			sum += shift_up(field, len);
-		else if (sign == 'q')
-			return 0;
-		else
-			flag = 0;
+		do{
+			char sign = input();
+		       	flag_sign = 1;
+			if (sign == 'a' || sign == 'A')
+				sum += shift_left(field, len);
+			else if (sign == 'd' || sign == 'D')
+				sum += shift_right(field, len);
+			else if (sign == 's' || sign == 'S')
+				sum += shift_down(field, len);
+			else if (sign == 'w' || sign == 'W')
+				sum += shift_up(field, len);
+			else if (sign == 'q')
+				return 0;
+			else
+				flag_sign = 0;
+		}while(!flag_sign);
 
-		clear(len + 3); 
-		if (!flag)
-			continue;
-
+		clear(len + 3);
 	 	rand_field(field, len);
 	}
 	return 0;
